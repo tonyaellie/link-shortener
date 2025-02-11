2cb548d8aa88473ee9c585d314711e53d152eae4
tonyaellie <tonya@tokia.dev>
2025-02-11 01:44:49 +0000

    fix: dont seem to need url?

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,9 +1,3 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient({
-  baseURL: window
-    ? window.location.origin
-    : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000",
-});
+export const authClient = createAuthClient();
