5c1002b4cb13a3d679eabfdabf31ddabe3be8d4e
tonyaellie <tonya@tokia.dev>
2025-02-11 01:41:27 +0000

    fix: try and fix url

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,7 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000",
+  baseURL: window.location.origin,
 });
